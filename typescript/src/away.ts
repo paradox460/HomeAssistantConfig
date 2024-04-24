fa1d80b4eb5b61e87719e32d936052aa03187639
Jeff Sandberg <paradox460@gmail.com>
2024-04-24 11:53:27 -0600

    Update away automations to use snapshot scenes
    Updates the away automations to make use of HomeAssistant's scene
    snapshot feature.
    
    Basically, when going away, a scene is created with a snapshot of _all_
    awayable entities current state.
    
    Upon returning home, the scene is reapplied, and then deleted

--- typescript/src/away.ts
+++ typescript/src/away.ts
@@ -1,8 +1,16 @@
 import { TServiceParams } from "@digital-alchemy/core";
+import { stringify } from "js-yaml";
 
 // 1 mile = 5,280 feet.
 const HOME_RADIUS = 5_280;
 
+/**
+ * Automated away and return home states
+ * 
+ * Turns off my furnace, and all entities with the `awayable` label applied.
+ * Saves a snapshot of the state of entities upon leaving, and restores it when
+ * returning home 
+ */
 export function Away({ hass, context, synapse }: TServiceParams) {
   const awaySwitch = synapse.switch({
     context,
@@ -20,12 +28,22 @@ export function Away({ hass, context, synapse }: TServiceParams) {
 
   function triggerAwayMode() {
     if (!awaySwitch.on) { return; }
+
+    const awayableEntities = hass.entity.byLabel("awayable");
+
     // trigger HVAC away
     hass.call.switch.turn_on({ entity_id: "switch.sandberg_system_manual_away_mode" });
 
+    // Take snapshot of all awayable entities current state
+    hass.call.scene.create({
+      scene_id: "awayable_restore",
+      snapshot_entities: stringify(awayableEntities)
+    })
+
+
     // Use the generic homeassistant.turn_off service because we might have a variety of things in the label
     // i.e. lights and fans and so forth
-    hass.call.homeassistant.turn_off({ entity_id: hass.entity.byLabel("awayable") });
+    hass.call.homeassistant.turn_off({ entity_id: awayableEntities });
 
     // Confusing because this is an occupany sensor, so awayMode true == home and false == away
     awayMode.on = false;
@@ -37,7 +55,18 @@ export function Away({ hass, context, synapse }: TServiceParams) {
     // force the system back into home mode
     if (!["towards", "arrived"].includes(direction) && awaySwitch.on) { return; }
 
+    // We have to cast the dynamically generated scene to a known, preconfigured scene, so the type checker passes
+    const awayableScene = "scene.awayable_restore" as "scene.enclosure_device_001_away_scene"
+
     hass.call.switch.turn_off({ entity_id: "switch.sandberg_system_manual_away_mode" });
+
+    // Restore the previous scene state
+    hass.call.scene.turn_on({
+      entity_id: awayableScene,
+      transition: 30
+    })
+    // And delete it so its not floating around
+    hass.call.scene.delete({ entity_id: awayableScene })
 
     awayMode.on = true;
   }
