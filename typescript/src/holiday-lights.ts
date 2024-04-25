23c4ced4bd1e5b69ccb1f13a3830dbbb8594cdf2
Jeff Sandberg <paradox460@gmail.com>
2024-04-25 11:40:31 -0600

    Port holiday lights out of NodeRed

---
+++ typescript/src/holiday-lights.ts
@@ -0,0 +1,78 @@
+import { CronExpression, sleep, TServiceParams } from "@digital-alchemy/core";
+
+export function HolidayLights({
+  automation,
+  context,
+  hass,
+  lifecycle,
+  scheduler,
+  synapse,
+}: TServiceParams) {
+  const manualSwitch = synapse.switch({
+    context,
+    defaultState: "off",
+    icon: "mdi:string-lights",
+    name: "Holiday Lights",
+  });
+
+  function turnOn() {
+    hass.call.homeassistant.turn_on({
+      entity_id: hass.entity.byLabel("holiday_lights"),
+    });
+    manualSwitch.on = true;
+  }
+
+  function turnOff() {
+    hass.call.homeassistant.turn_off({
+      entity_id: hass.entity.byLabel("holiday_lights"),
+    });
+    manualSwitch.on = false;
+  }
+
+  manualSwitch.onUpdate(() => {
+    manualSwitch.on ? turnOn() : turnOff();
+  });
+
+  automation.solar.onEvent({
+    context,
+    eventName: "sunsetStart",
+    exec() {
+      turnOn();
+    },
+  });
+
+  scheduler.sliding({
+    exec() {
+      turnOn();
+    },
+    next() {
+      return automation.solar.nightEnd.subtract(1, "hour");
+    },
+    reset: CronExpression.EVERY_DAY_AT_2AM,
+  });
+
+  automation.solar.onEvent({
+    context,
+    eventName: "sunriseEnd",
+    async exec() {
+      await sleep(60 * 1000 * 60);
+      turnOff();
+    },
+  });
+
+  // Force state sync for manual control, to ensure the switch reflects reality
+  // at boot
+  lifecycle.onReady(() => {
+    if (
+      hass.entity
+        .byLabel("holiday_lights")
+        .some(entity => hass.entity.byId(entity).state == "on")
+    ) {
+      turnOn();
+      manualSwitch.on = true;
+    } else {
+      turnOff();
+      manualSwitch.on = false;
+    }
+  });
+}
